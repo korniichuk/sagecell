df70057e67222b86de0c5bc1585e85200f565f0f
Ruslan Korniichuk <ruslan.korniichuk@gmail.com>
2015-06-18 13:54:29 +0200

    Fix 'sagecell -v' command

--- sagecell/sagecell.py
+++ sagecell/sagecell.py
@@ -171,7 +171,7 @@ def parse_command_line_args():
     parser = ArgumentParser(prog="sagecell", description=argparse["_parser"],
                             add_help=True)
     parser.add_argument("-v", "--version", action="version",
-                        version="sagecell 0.1a")
+                        version="sagecell 0.1")
     # Create subparsers for the top parser
     subparsers = parser.add_subparsers(title=argparse["_subparsers"])
     # Create the parser for the "install" subcommand
