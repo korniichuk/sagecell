4799906b07106727632df1b559fecd271a48384b
Ruslan Korniichuk <ruslan.korniichuk@gmail.com>
2015-06-25 14:59:03 +0200

    Fix fabric file

--- fabfile.py
+++ fabfile.py
@@ -8,21 +8,18 @@ from fabric.api import local
 def live():
     """Upload package to PyPI Live"""
 
-    local("cd /home/paad/sagecell; "
-          "python setup.py register -r pypi; "
-          "python setup.py sdist --format=zip,gztar upload -r pypi")
+    local("python setup.py register -r pypi")
+    local("python setup.py sdist --format=zip,gztar upload -r pypi")
 
 def git():
     """Setup git"""
 
-    local("cd /home/paad/sagecell; "
-          "git remote rm origin; "
-          "git remote add origin https://korniichuk@github.com/korniichuk/sagecell.git; "
-          "git remote add bitbucket https://korniichuk@bitbucket.org/korniichuk/sagecell.git")
+    local("git remote rm origin")
+    local("git remote add origin https://korniichuk@github.com/korniichuk/sagecell.git")
+    local("git remote add bitbucket https://korniichuk@bitbucket.org/korniichuk/sagecell.git")
 
 def test():
     """Upload package to PyPI Test"""
 
-    local("cd /home/paad/sagecell; "
-          "python setup.py register -r pypitest; "
-          "python setup.py sdist --format=zip,gztar upload -r pypitest")
+    local("python setup.py register -r pypitest")
+    local("python setup.py sdist --format=zip,gztar upload -r pypitest")
