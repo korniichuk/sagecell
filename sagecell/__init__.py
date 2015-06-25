570b19fb35b1a2ddc05580cc1d7f82a659969a8b
Ruslan Korniichuk <ruslan.korniichuk@gmail.com>
2015-06-25 21:00:21 +0200

    Prepare the sagecell ver. 0.2a1 for the PyPI

--- sagecell/__init__.py
+++ sagecell/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '0.1b3'
+__version__ = '0.2a1'
--- sagecell/sagecell.py
+++ sagecell/sagecell.py
@@ -165,7 +165,7 @@ def parse_command_line_args():
     parser = ArgumentParser(prog="sagecell", description=argparse["_parser"],
                             add_help=True)
     parser.add_argument("-v", "--version", action="version",
-                        version="sagecell 0.1b3")
+                        version="sagecell 0.2a1")
     # Create subparsers for the top parser
     subparsers = parser.add_subparsers(title=argparse["_subparsers"])
     # Create the parser for the "install" subcommand
