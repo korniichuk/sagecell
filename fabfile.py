b94e15496e8c0053da255dad133a6b7ed643902a
Ruslan Korniichuk <ruslan.korniichuk@gmail.com>
2015-06-25 15:24:54 +0200

    Sort functions in fabfile.py file

--- fabfile.py
+++ fabfile.py
@@ -5,18 +5,18 @@
 
 from fabric.api import local
 
-def live():
-    """Upload package to PyPI Live"""
-
-    local("python setup.py register -r pypi")
-    local("python setup.py sdist --format=zip,gztar upload -r pypi")
-
 def git():
     """Setup git"""
 
     local("git remote rm origin")
     local("git remote add origin https://korniichuk@github.com/korniichuk/sagecell.git")
     local("git remote add bitbucket https://korniichuk@bitbucket.org/korniichuk/sagecell.git")
+
+def live():
+    """Upload package to PyPI Live"""
+
+    local("python setup.py register -r pypi")
+    local("python setup.py sdist --format=zip,gztar upload -r pypi")
 
 def test():
     """Upload package to PyPI Test"""
