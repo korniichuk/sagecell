1e3b2d90df8baeb17b8e5f822a6661d25988834d
Ruslan Korniichuk <ruslan.korniichuk@gmail.com>
2015-07-20 12:26:16 +0200

    Prepare the sagecell ver. 0.2b5 for the PyPI

--- sagecell/__init__.py
+++ sagecell/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '0.2b4'
+__version__ = '0.2b5'
--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     platforms = ["Linux"],
     scripts=['scripts/sagecellscript'],
     url = "https://github.com/korniichuk/sagecell",
-    version = "0.2b4",
+    version = "0.2b5",
     zip_safe = True
 )
